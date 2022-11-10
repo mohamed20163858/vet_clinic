b82385b73798adf4eb8369bfc9467ff55ae1043e
mohamed saleh <mohamed20163858@gmail.com>
2022-11-11 01:02:44 +0200

    Finish exercise-2

--- queries.sql
+++ queries.sql
@@ -8,3 +8,51 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg>10.5;
 SELECT * FROM animals WHERE neutered=true;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 SELECT * FROM animals WHERE weight_kg>=10.4 AND weight_kg<=17.3;
+
+-- begin a transaction
+BEGIN;
+UPDATE animals SET species='unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+BEGIN;
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
+SELECT * FROM animals;
+UPDATE animals SET species='pokemon' WHERE name NOT LIKE '%mon';
+SELECT * FROM animals;
+COMMIT;
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+BEGIN;
+DELETE FROM animals WHERE date_of_birth>'2022-01-01';
+SELECT * FROM animals;
+SAVEPOINT s1;
+UPDATE animals SET weight_kg=weight_kg*-1;
+SELECT * FROM animals;
+ROLLBACK TO s1;
+SELECT * FROM animals;
+BEGIN;
+UPDATE animals SET weight_kg=weight_kg*-1 WHERE weight_kg<0;
+SELECT * FROM animals;
+COMMIT;
+-- How many animals are there?
+SELECT COUNT(*) FROM animals;
+-- How many animals have never tried to escape?
+SELECT COUNT(*) FROM animals WHERE escape_attempts=0;
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+-- Who escapes the most, neutered or not neutered animals?
+SELECT SUM(escape_attempts) FROM animals WHERE neutered=true;
+SELECT SUM(escape_attempts) FROM animals WHERE neutered=false;
+-- it is clear that neutered animals escaped the most
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT MIN(weight_kg),MAX(weight_kg) FROM animals WHERE species='digimon';
+SELECT MIN(weight_kg),MAX(weight_kg) FROM animals WHERE species='pokemon';
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT AVG(escape_attempts) FROM animals WHERE species='digimon' AND date_of_birth BETWEEN '1990-01-01' AND '2000-01-01';
+SELECT AVG(escape_attempts) FROM animals WHERE species='pokemon' AND date_of_birth BETWEEN '1990-01-01' AND '2000-01-01';
