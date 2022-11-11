c9100b2504659b2a1f8851da62dfad3308286c46
mohamed saleh <mohamed20163858@gmail.com>
2022-11-11 18:20:42 +0200

    Finish exercise-3

--- data.sql
+++ data.sql
@@ -12,3 +12,25 @@ INSERT INTO animals(id, name, date_of_birth, weight_kg, neutered, escape_attempt
 INSERT INTO animals(id, name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (9, 'Boarmon', '2005-06-07', 20.4, true, 7);
 INSERT INTO animals(id, name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (10, 'Blossom', '1998-10-13', 17, true, 3);
 INSERT INTO animals(id, name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (11, 'Ditto', '2022-05-14', 22, true, 4);
+
+INSERT INTO owners(full_name, age) VALUES('Sam Smith', 34);
+INSERT INTO owners(full_name, age) VALUES('Jennifer Orwell', 19);
+INSERT INTO owners(full_name, age) VALUES('Bob', 45);
+INSERT INTO owners(full_name, age) VALUES('Melody Pond', 77);
+INSERT INTO owners(full_name, age) VALUES('Dean Winchester', 14);
+INSERT INTO owners(full_name, age) VALUES('Jodie Whittaker', 38);
+
+INSERT INTO species(name) VALUES('Pokemon');
+INSERT INTO species(name) VALUES('Digimon');
+
+UPDATE animals SET species_id=(SELECT id FROM species WHERE name='Digimon') WHERE name LIKE '%mon';
+UPDATE animals SET species_id=(SELECT id FROM species WHERE name='Pokemon') WHERE name NOT LIKE '%mon';
+
+UPDATE animals SET owner_id=(SELECT id FROM owners WHERE full_name='Sam Smith') WHERE  name='Agumon';
+UPDATE animals SET owner_id=(SELECT id FROM owners WHERE full_name='Jennifer Orwell') WHERE  name='Gabumon' OR name='Pikachu';
+UPDATE animals SET owner_id=(SELECT id FROM owners WHERE full_name='Bob') WHERE  name='Devimon' OR name='Plantmon';
+UPDATE animals SET owner_id=(SELECT id FROM owners WHERE full_name='Melody Pond') WHERE  name='Charmander' OR name='Squirtle' OR name='Blossom';
+UPDATE animals SET owner_id=(SELECT id FROM owners WHERE full_name='Dean Winchester') WHERE  name='Angemon' OR name='Boarmon';
+
+
+
